c813e15fb176a16cc6bcadf13a93b391e03adfdf
Vinit Upadhyay <vinitupadhyay994@gmail.com>
2025-02-16 16:20:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -20,7 +20,7 @@ int main() {
             if (num2 != 0) {
                 printf("%d", num1 / num2);
             } else {
-                printf("Error! Division by zero is not allowed.\n");
+                printf("error");
             }
             break;
         case '%': 
