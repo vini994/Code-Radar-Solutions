6bbcf394a9e1f456f6c4cae44875f0c05cee960c
Vinit Upadhyay <vinitupadhyay994@gmail.com>
2025-02-16 16:19:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 
 int main() {
-    int num1,num2;
+    int num1, num2;
     char op;
-    scanf("%d %d %c",&num1,&num2,&op); 
+    scanf("%d %d %c", &num1, &num2, &op);  
 
-    // Perform the operation
-    switch(op){
+
+    switch (op) {
         case '+': 
-            printf(num1 + num2);
+            printf("%d", num1 + num2);
             break;
         case '-': 
-            printf(num1 - num2);
+            printf("%d", num1 - num2);
             break;
         case '*': 
-            printf(num1 * num2);
+            printf("%d", num1 * num2);
             break;
         case '/': 
-            if (num2 != 0){
-                printf(num1 / num2);
-            }else{
-                printf("Error! Division by zero is not allowed");
-            }    
+            if (num2 != 0) {
+                printf("%d", num1 / num2);
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+            }
             break;
         case '%': 
-            if (num2 != 0){
-                printf(num1 % num2);
-            }else{
+            if (num2 != 0) {
+                printf("%d", num1 % num2);
+            } else {
                 printf("Error! Modulo by zero is not allowed.\n");
-            } 
+            }
             break;
         default: 
-            printf("Invalid operator! Please use +, -, *, /, or %");
+            printf("Invalid operator! Please use +, -, *, /, or %%.\n");
     }
+
     return 0;
 }
